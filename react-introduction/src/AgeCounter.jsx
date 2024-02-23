a86455748470adb02d9fd9f9dc6ad29e17da2fc7
Boris Prochnau <boris.prochnau@colenio.com>
2024-02-23 18:53:36 +0100

    feat: extract seperate age display component

--- react-introduction/src/AgeCounter.jsx
+++ react-introduction/src/AgeCounter.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AgeDisplay } from "./AgeDisplay";
 
 export function AgeCounter(props) {
   const [age, setAge] = useState(30);
@@ -9,8 +10,7 @@ export function AgeCounter(props) {
 
   return (
     <div>
-      <h1>Age Counter</h1>
-      <p>Age: {age}</p>
+      <AgeDisplay age={age} />
       <button onClick={increaseAge}>Increase Age</button>
     </div>
   );
---
+++ react-introduction/src/AgeDisplay.jsx
@@ -0,0 +1,8 @@
+export function AgeDisplay(props) {
+  return (
+    <div>
+      <h1>Age Display</h1>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+}
