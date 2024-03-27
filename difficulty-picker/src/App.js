8ccde97b4b047d83894e62a388cfcbf91e7e5496
BorisP <Boris.Prochnau@mail.de>
2024-03-27 22:41:25 +0100

    refactor(difficutlty-picker): externalize difficulty list

--- difficulty-picker/src/App.js
+++ difficulty-picker/src/App.js
@@ -2,6 +2,7 @@ import DisplayDifficulty from "./components/DisplayDifficulty/DisplayDifficulty"
 import MenuList from "./components/MenuList/MenuList";
 import s from "./style.module.css";
 import { useState } from "react";
+import { DIFFICULTIES } from "./components/MenuList/constants";
 
 export function App() {
   const [currentDifficulty, setCurrentDifficulty] = useState("Insane");
@@ -15,7 +16,7 @@ export function App() {
       <div>
         <h1>Select your difficulty</h1>
         <div className={s.workspace}>
-          <MenuList currentDifficulty={currentDifficulty} items={["Low", "Medium", "Hard", "Insane"]} onItemClick={onMenuListItemClick} />
+          <MenuList currentDifficulty={currentDifficulty} items={DIFFICULTIES} onItemClick={onMenuListItemClick} />
           <DisplayDifficulty difficulty={currentDifficulty} />
         </div>
       </div>
